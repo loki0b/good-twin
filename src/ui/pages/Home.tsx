e46a7d65c46a776d8f47cb109aa9eca86333d74a
loki0b <contato@henriquegarcialeite.com>
2026-06-17 14:00:18 -0300

    test: works

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -1,17 +1,6 @@
-import { writeGreenLeds } from "../lib/fpgaApi.js"
-
-let data = 0x00000000
-
 function Home() {
-    const writeGreen = async () => {
-        if (data === 0x00000000) data = 0x0000000F;
-        else data = 0x00000000;
-
-        await writeGreenLeds(data);
-    }
-
     return (
-        <button onClick={writeGreen}>Clique!</button>
+        <button>Clique!</button>
     )
 }
 
